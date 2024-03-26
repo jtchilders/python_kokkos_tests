6f85047f067c0546b12ad874e6152e0d48e12294
Taylor Childers <taylorchilders@gmail.com>
2024-03-26 11:57:47 -0400

    adding vscode settings; adding kokkos example

--- example.cpp
+++ example.cpp
@@ -3,23 +3,34 @@
 #include "Kokkos_Core.hpp"
 
 namespace py = pybind11;
-using kvint = Kokkos::View<int*>;
+using kvint = Kokkos::View<int*,Kokkos::DefaultExecutionSpace>;
 
-int add(int i = 1, int j = 2) {
-   return i + j;
+// function to initialize kokkos
+void init(){
+   Kokkos::initialize();
 }
 
+// function to finalize kokkos
+void finalize(){
+   Kokkos::finalize();
+}
 
 PYBIND11_MODULE(example, m) {
-
-   Kokkos::ScopeGuard ksg;
-
-   kvint view("view", 10);
    
-   m.doc() = "pybind11 example plugin"; // optional module docstring
+   m.doc() = "pybind11 example with Kokkos"; // optional module docstring
 
-   m.def("add", &add, "A function which adds two numbers",
-   py::arg("i") = 1, py::arg("j") = 2);
+   // call the init and finalize functions
+   m.def("init", &init);
+   m.def("finalize", &finalize);
 
+   // create a pybind class for the kvint View object
+   py::class_<kvint>(m, "KV_Int")
+      // initialize the kvint object with an extent
+      .def(py::init<std::string,const int>())
+      // expose the extent method which takes an integer as an argument
+      .def("extent", [](const kvint &v, int dim) { return v.extent(dim); })
+      // provide get and set functions for the view
+      .def("set", [](const kvint& v, int i, int val) { v(i) = val; })
+      .def("get", [](const kvint& v, int i) { return v(i); });
 
 }

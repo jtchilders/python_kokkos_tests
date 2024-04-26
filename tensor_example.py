ad8abfde34683059394a6752a6b77e13f9117f45
Taylor Childers <taylor@jtchilders.com>
2024-04-26 12:13:54 +0000

    clarification

--- tensor_example.py
+++ tensor_example.py
@@ -1,24 +1,9 @@
 import torch
 import tenex
 import ctypes
-import torch.utils.dlpack as thdlpack
 
 tenex.init()
 tenex.print_hw_config()
-
-
-def wrap_in_tensor2(data_ptr, size):
-   # Convert uintptr_t to a proper device pointer for PyTorch
-   buffer = (ctypes.c_float * size).from_address(data_ptr)
-   storage = torch.cuda.FloatStorage.from_buffer(buffer, size)
-   tensor = torch.FloatTensor(storage).view(size)
-   return tensor
-
-
-def wrap_in_tensor(data_ptr, size):
-   
-   tensor = thdlpack.from_dlpack(data_ptr)
-   return tensor
 
 # Create a tensor on a device (e.g., CUDA)
 tensor = torch.randn(10, device='cuda')  # Example tensor
@@ -28,8 +13,7 @@ print('starting tensor: ', tensor)
 result = tenex.process_tensor(tensor.data_ptr(), tensor.size(0))
 
 # Wrap the result pointer in a PyTorch tensor
-output_tensor = wrap_in_tensor(result.data_ptr, result.size)
-print('result tensor: ', output_tensor)
-
+# something like this
+# output = torch.Tensor(result.data_ptr,result.size)
 
 tenex.finalize()
